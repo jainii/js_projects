2208f1b31887536c1286269ae0b92dbf85161512
jarvis-cmd <17dce045@charusat.edu.in>
2020-08-21 23:45:51 +0530

    upto challenge 2

---
+++ static/js/script.js
@@ -0,0 +1,22 @@
+// chalange 1: your age in days
+
+function ageInDays(){
+    var birthyear = prompt('What year were you born ... Good friend?');
+    var ageInDayss = (2020 - birthyear) * 365;
+    var h1 = document.createElement('h1');
+    var textAnswer = document.createTextNode('You are ' + ageInDayss +' days old !!!');
+    h1.setAttribute('id','ageInDays');
+    h1.appendChild(textAnswer);
+    document.getElementById('flex-box-result').appendChild(h1);
+}
+
+function reset() {
+    document.getElementById('ageInDays').remove();
+}
+
+function generateCat(){ 
+    var image = document.createElement('img');
+    var div = document.getElementById('flex-cat-gen');
+    image.src = "http://thecatapi.com/api/images/get?format=src&type=gif&size=small";
+    div.appendChild(image);
+}
